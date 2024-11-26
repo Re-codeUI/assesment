579f4cc6be7f60e1600bece136766b23afc64ef9
Re-codeUI <recodeui@gmail.com>
2024-11-26 10:34:47 +0700

    remove roles

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,6 @@ Route::group(['prefix' => 'users'], function(){
     Route::delete('destroy/{user}', [App\Http\Controllers\User\UserController::class, 'destroy'])->name('users.destroy');
 });
 
-Route::group(['prefix' => 'roles'], function(){
-    Route::get('index', [App\Http\Controllers\Role\RoleController::class, 'index'])->name('roles');
-    Route::get('create', [App\Http\Controllers\Role\RoleController::class, 'create'])->name('roles.create');
-    Route::post('store', [App\Http\Controllers\Role\RoleController::class, 'store'])->name('roles.store');
-    Route::get('show/{role}', [App\Http\Controllers\Role\RoleController::class, 'show'])->name('roles.show');
-    Route::get('edit/{role}', [App\Http\Controllers\Role\RoleController::class, 'edit'])->name('roles.edit');
-    Route::put('update/{role}', [App\Http\Controllers\Role\RoleController::class, 'update'])->name('roles.update');
-    Route::delete('destroy/{role}', [App\Http\Controllers\Role\RoleController::class, 'destroy'])->name('roles.destroy');
-});
-
 Route::group(['prefix' => 'questions'], function(){
     Route::get('index', [App\Http\Controllers\Question\QuestionController::class, 'index'])->name('questions');
     Route::get('create', [App\Http\Controllers\Question\QuestionController::class, 'create'])->name('questions.create');
